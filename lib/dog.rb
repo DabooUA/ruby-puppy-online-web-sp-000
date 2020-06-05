a0f17402190efea222934c1f1cc6b643ee186f9a
Serhiy Bardysh <sbardysh@tutanota.com>
2020-06-05 02:44:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,12 +7,16 @@ attr_accessor :name
 def initialize(name)
   @name = name
   @@all << self
-  #self.save
+  self.save
 end
 
+
+
 def self.all
-  @all
-end
+   @@all.each do |dog|
+     puts dog.name
+   end
+ end
 
  def self.clear_all
    @@all.clear
